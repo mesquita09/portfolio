58ae7e0fb4463598e2798357ce7499b644310b48
Adelson Mesquita <mesquita.adelson@gmail.com>
2020-01-29 21:50:22 -0300

    contact favicon profile update
    update nos textos de contato e imagens

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,19 +11,19 @@ const Contact = () => {
   return (
     <section id="contact">
       <Container>
-        <Title title="Contact" />
+        <Title title="Contato" />
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
             <p className="contact-wrapper__text">
-              {cta || 'Would you like to work with me? Awesome!'}
+              {cta || 'O que acha de tirar seu projeto do papel?'}
             </p>
             <a
               target="_blank"
               rel="noopener noreferrer"
               className="cta-btn cta-btn--resume"
-              href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
+              href={email ? `mailto:${email}` : 'https://indicempresarial.com'}
             >
-              {btn || "Let's Talk"}
+              {btn || 'Vamos conversar!'}
             </a>
           </div>
         </Fade>
