46da5944a362f72fcf9a263938c07ec1bd241bbc
Adelson Mesquita <mesquita.adelson@gmail.com>
2020-01-29 21:41:48 -0300

    Update Footer.jsx
    desativa o git hub buttons do footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react';
 import { Container } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
-import GithubButtons from '../GithubButtons/GithubButtons';
+// import GithubButtons from '../GithubButtons/GithubButtons';
 
-import { githubButtons } from '../../mock/data';
+// import { githubButtons } from '../../mock/data';
 
 const Footer = () => {
   const { footer } = useContext(PortfolioContext);
   const { networks } = footer;
-  const { isEnabled } = githubButtons;
+  // const { isEnabled } = githubButtons;
 
   return (
     <footer className="footer navbar-static-bottom">
@@ -35,13 +35,13 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
-          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
+          © {new Date().getFullYear()} - Developed by{' '}
+          <a href="https://indicempresarial.com" target="_blank" rel="noopener noreferrer">
+            Indice Digital
           </a>
         </p>
 
-        {isEnabled && <GithubButtons />}
+        {/* isEnabled && <GithubButtons /> */}
       </Container>
     </footer>
   );
