c8e42bce5d75542c48c2ce60b4a193e6c89a9ba8
Adelson Mesquita <mesquita.adelson@gmail.com>
2020-03-30 17:26:44 -0300

    mailchimp connection test

--- src/components/Mailchimp/index.js
+++ src/components/Mailchimp/index.js
@@ -1,6 +1,4 @@
-import { Component } from 'react';
-
-class Mailchimp extends Component {
+/* class Mailchimp extends Component {
   componentDidMount() {
     const script = document.createElement('script');
     script.async = true;
@@ -9,6 +7,24 @@ class Mailchimp extends Component {
     // For head
     document.head.appendChild(script);
   }
-}
+} */
+
+const Mailchimp = callback => {
+  const existingScript = document.getElementById('mcjs');
+
+  if (!existingScript) {
+    const script = document.createElement('script');
+    script.src =
+      'https://chimpstatic.com/mcjs-connected/js/users/6d2cf372e89cc2d8f4fbadda8/e1ed7dcea42f13dceef0f5f6e.js';
+    script.id = 'mcjs';
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      if (callback) callback();
+    };
+  }
+
+  if (existingScript && callback) callback();
+};
 
 export default Mailchimp;
--- src/html.js
+++ src/html.js
@@ -20,7 +20,7 @@ export default function HTML({
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <div id="mcjs">{Mailchimp}</div>
+        <Mailchimp />
         {headComponents}
       </head>
       <body {...bodyAttributes}>
